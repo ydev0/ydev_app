abbf66db30ed4d9b9eda8a275e203d522d5053cc
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-05-05 11:30:04 -0300

    started login

--- src/main/java/com/ydev00/controller/UserController.java
+++ src/main/java/com/ydev00/controller/UserController.java
@@ -2,13 +2,17 @@ package com.ydev00.controller;
 
 import spark.Route;
 import static spark.Spark.*;
+
 import java.sql.Connection;
+import com.google.gson.Gson;
+
 import com.ydev00.model.User;
 import com.ydev00.dao.UserDAO;
 
 public class UserController {
   private User user;
   private Connection dbConn;
+  private Gson gson;
 
   public UserController() {}
 
@@ -19,12 +23,15 @@ public class UserController {
 
   public Route login = (request, response) -> {
     response.type("application/json");
+    
     Route route = null;
     user = null;
 
-        
 
     UserDAO userDAO = new UserDAO(dbConn);
+
+    user = (User)userDAO.getById();
+
     return route;
   };
 }
--- src/main/java/com/ydev00/model/User.java
+++ src/main/java/com/ydev00/model/User.java
@@ -1,26 +1,41 @@
 package com.ydev00.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+  @SerializedName("id")
   private int id;       
+
+  @SerializedName("name")
   private String name;   
+
+  @SerializedName("userName")
   private String userName;
+  
+  @SerializedName("password")
   private String password; 
+
+  @SerializedName("isLogged")
   private boolean isLogged; 
+
+  @SerializedName("profilePic")
+  private Image profilePic;
 
   //constructors
   public User() {
   }
 
-  public User(int id, String name, String userName, String password) {
+  public User(int id, String name, String userName, String password, Image profilePic) {
     this.id = id;
     this.name = name;
     this.userName = userName;
     this.password = password;
+    this.profilePic = profilePic;
   }
 
   // methods
   public User createUser() {
-    User user = new User(0, "Carlos", "CarlosRoot", "password");
+    User user = new User(0, "Carlos", "CarlosRoot", "password", null);
     user.isLogged = true;
     return user;
   } 
@@ -34,12 +49,16 @@ public class User {
     this.name = name;
   }
 
-  public void setUserName(String userName) {
+  public void setUsername(String userName) {
     this.userName = userName;
   }
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public void setProfilePic(Image pfp) {
+    this.profilePic = pfp;
   }
 
   public int getId() {
