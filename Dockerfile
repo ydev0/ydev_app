1b8956a4b67b8ffcec3612d8ef9c30e9faad788a
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-06-03 10:10:36 -0300

    projected for docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM --platform=$BUILDPLATFORM maven:3.9.6-eclipse-temurin-21 AS build 
+FROM maven:3.9.6-eclipse-temurin-22 AS build 
 WORKDIR /workdir/server
+
 COPY pom.xml /workdir/server/pom.xml
+
 RUN mvn dependency:go-offline
+
 COPY src /workdir/server/src
-RUN mvn --batch-mode clean compile assembly:single
+
+  RUN mvn --batch-mode clean compile assembly:single
 
 FROM build AS dev-envs
 
@@ -14,10 +18,11 @@ RUN useradd -s /bin/bash -m vimuser && \
     groupadd docker && \
     usermod -aG docker vimuser
 
-# install Docker tools (cli, buildx, compose)
-COPY --from=gloursdocker/docker / /
+ENV JAR_FILE=${JAR_FILE} 
 
-ARG JAR_FILE
-COPY target/ydev-0.0.1-jar-with-dependencies.jar /target/ydev-0.0.1-jar-with-dependencies.jar
-CMD java -jar /target/ydev-0.0.1-jar-with-dependencies.jar
+COPY --from=build /workdir/server/target/${JAR_FILE} /${JAR_FILE}
+
+CMD java -jar /${JAR_FILE}
+
 EXPOSE 8080
+
--- src/main/java/com/ydev00/util/DBServer.java
+++ src/main/java/com/ydev00/util/DBServer.java
@@ -6,13 +6,27 @@ public class DBServer {
   private Connection conn;
 
   public DBServer() {
-    try {
-      conn = DriverManager.getConnection("jdbc:mysql://localhost/ydev_db?" + "user=root&password=password");
-    } catch (SQLException ex) {
-      // handle any errors
-      System.out.println("SQLException: " + ex.getMessage());
-      System.out.println("SQLState: " + ex.getSQLState());
-      System.out.println("VendorError: " + ex.getErrorCode());
+    int retries = 5;
+    while (retries > 0) {
+      try {
+        conn = DriverManager.getConnection("jdbc:mysql://ydev_db:3306/ydev_db?" + "user=root&password=password");
+        break;
+      } catch (SQLException ex) {
+        retries--;
+        System.out.println("Failed to connect to database. Retrying in 5 seconds...");
+        System.out.println("SQLException: " + ex.getMessage());
+        System.out.println("SQLState: " + ex.getSQLState());
+        System.out.println("VendorError: " + ex.getErrorCode());
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }  
+      if (retries == 0) {
+        System.out.println("Failed to connect to database. Exiting...");
+        System.exit(1);
+      }
     }
   }
 
